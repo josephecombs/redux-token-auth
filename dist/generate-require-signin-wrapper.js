68ae485be15e90653391e0e2b01e64f6d514f9a6
Leo <leobrionesherrera@gmail.com>
2021-02-23 13:44:38 -0600

    Added conole.logs

--- dist/generate-require-signin-wrapper.js
+++ dist/generate-require-signin-wrapper.js
@@ -1,8 +1,11 @@
 "use strict";
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -22,6 +25,7 @@ var generateRequireSignInWrapper = function (_a) {
             }
             GatedPage.prototype.UNSAFE_componentWillReceiveProps = function (nextProps) {
                 var history = nextProps.history, hasVerificationBeenAttempted = nextProps.hasVerificationBeenAttempted, isSignedIn = nextProps.isSignedIn;
+                console.log(history, hasVerificationBeenAttempted, isSignedIn);
                 if (hasVerificationBeenAttempted && !isSignedIn) {
                     history.replace(redirectPathIfNotSignedIn);
                 }
--- src/generate-require-signin-wrapper.tsx
+++ src/generate-require-signin-wrapper.tsx
@@ -24,6 +24,7 @@ const generateRequireSignInWrapper = (
           hasVerificationBeenAttempted,
           isSignedIn,
         } = nextProps
+        console.log(history, hasVerificationBeenAttempted, isSignedIn)
         if (hasVerificationBeenAttempted && !isSignedIn) {
           history.replace(redirectPathIfNotSignedIn)
         }
